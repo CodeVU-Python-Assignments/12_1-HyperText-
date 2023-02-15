827fb6139ef13d7c4a848c2378121d9781bb81f1
Nathan Gearke <69879313+GearsandKeys@users.noreply.github.com>
2023-02-15 10:43:57 -0600

    Update socket1.py

--- socket1.py
+++ socket1.py
@@ -1,8 +1,9 @@
 import socket
 
-def getWebData():
+def assignment_12_1():
     mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     mysock.connect(('data.pr4e.org', 80))
+    # Make sure to change this url
     cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
     mysock.send(cmd)
 
@@ -13,4 +14,6 @@ def getWebData():
         print(data.decode(),end='')
 
     mysock.close()
-##getWebData()
+
+if __name__ == "__main__":    
+    assignment_12_1()
